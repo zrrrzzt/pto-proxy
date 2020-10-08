e575732f8a96f4ec4714c404c24f25446d3fc82c
Alexander Gård <alex.gaard@hotmail.com>
2020-10-08 15:31:27 +0200

    Fjernet proxy controller

--- src/test/java/no/nav/pto_proxy/config/TestApplicationConfig.java
+++ src/test/java/no/nav/pto_proxy/config/TestApplicationConfig.java
@@ -1,7 +1,9 @@
 package no.nav.pto_proxy.config;
 
+import no.nav.common.log.LogFilter;
 import no.nav.pto_proxy.filter.PostRequestZuulFilter;
 import no.nav.pto_proxy.filter.PreRequestZuulFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Import;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static no.nav.common.utils.EnvironmentUtils.isDevelopment;
 
 @EnableZuulProxy
 @Configuration
@@ -24,11 +28,20 @@ public class TestApplicationConfig {
     }
 
     @Bean
+    public FilterRegistrationBean logFilterRegistrationBean() {
+        FilterRegistrationBean<LogFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new LogFilter("pto-proxy", isDevelopment().orElse(false)));
+        registration.setOrder(1);
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
+
+    @Bean
     public PreRequestZuulFilter proxyPreFilter() {
         Map<String, String> keyMap = new HashMap<>();
         keyMap.put("test-app", "test-key");
 
-        return new PreRequestZuulFilter("/proxy2", keyMap);
+        return new PreRequestZuulFilter("/proxy", keyMap);
     }
 
     @Bean
--- src/test/java/no/nav/pto_proxy/config/TestControllerConfig.java
+++ src/test/java/no/nav/pto_proxy/config/TestControllerConfig.java
@@ -1,10 +1,9 @@
 package no.nav.pto_proxy.config;
 
 import no.nav.pto_proxy.ProxySinkController;
-import no.nav.pto_proxy.controller.ApiGwProxyController;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ApiGwProxyController.class, ProxySinkController.class})
+@Import({ProxySinkController.class})
 public class TestControllerConfig {}
